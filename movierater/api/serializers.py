eee095e0177b78c619ab5eda77b8f6875a0dd9fa
Alejandro <alejandroalvarez1997@hotmail.com>
2018-09-27 12:48:47 -0500

    Back End

--- movierater/api/serializers.py
+++ movierater/api/serializers.py
@@ -6,7 +6,21 @@ from api.models import Movie, Rating
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+        # write_only_fields = ('password',)
+        read_only_fields = ('id',)
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
